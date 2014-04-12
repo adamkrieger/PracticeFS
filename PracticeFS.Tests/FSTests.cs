b89db5a0b3bcf8db92f0a41b5861ad4f306f2306
adamkrieger <adamkrieger.ca@gmail.com>
2014-04-11 21:30:39 -0500

    Initial commit. One type test, one function test.

---
+++ PracticeFS.Tests/FSTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeFS.Tests
+{
+	[TestFixture]
+    public class FSTests
+    {
+		[Test]
+		public void DeclaringAPersonReturnsFirstName()
+		{
+			var person = new FSSource.Person("Adam","Krieger");
+
+			var firstName = person.First;
+
+			Assert.AreEqual("Adam", firstName);
+		}
+
+		[Test]
+		public void MultiplyingValuesWorksAsExpected()
+		{
+			var x = 4;
+			var y = 5;
+
+			var result = FSSource.multip(x, y);
+
+			Assert.AreEqual(20, result);
+		}
+
+    }
+}
---
+++ PracticeFS/FSSource.fs
@@ -0,0 +1,5 @@
+﻿module PracticeFS.FSSource
+
+type Person = {First:string; Last:string}
+
+let multip x y = x * y
